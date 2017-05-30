49e952d64a29d3b7a0279032d9cd9e768f4b28a8
jordanbrauer <jbrauer.inc@gmail.com>
2017-05-29 23:45:38 -0500

    WIP: Application boilerplate code

---
+++ src/app/components/App.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Todo List App</h1>
+      </div>
+    );
+  }
+}
